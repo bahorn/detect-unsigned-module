4a10ac2563199e6d5a265646ab8801f72f8760a7
B Horn <b@horn.uk>
2025-10-15 14:34:56 +0000

    fix random slash that got added
    Signed-off-by: B Horn <b@horn.uk>

--- baselining/baseline.sh
+++ baselining/baseline.sh
@@ -2,5 +2,5 @@
 sudo rm -r a b
 sudo python3 system_snapshot.py a.baseline
 sleep 5
-sudo python3 system_snap/shot.py b.baseline
+sudo python3 system_snapshot.py b.baseline
 diff -r a.baseline b.baseline | grep "diff -r" | python3 get_ignore_list.py /dev/stdin
