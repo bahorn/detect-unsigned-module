bba5949b126f17a2a8ff527dbb4d8526a122fc3c
B Horn <b@horn.uk>
2025-12-12 02:15:27 +0000

    hex writes in bad_ftrace_value.py
    real implementation supports it, but not singularity
    
    Signed-off-by: B Horn <b@horn.uk>

--- detections/bad_ftrace_value.py
+++ detections/bad_ftrace_value.py
@@ -6,25 +6,42 @@ if you write a random value to any file on disk called `ftrace_enabled` all
 other files with the same name will have the same content.
 you can detect this behaviour by writing to /tmp/ftrace_enabled and then try to
 read the sysctl() version.
+
+singularity atm doesn't support hex constants, so we can exploit that for
+detection.
 """
-
-VALUE = b'9999999999999999999'
-
-try:
-    with open('/proc/sys/kernel/ftrace_enabled', 'wb') as f:
-        f.write(VALUE)
-except OSError:
-    print('not tampering with ftrace_enabled')
-    exit()
-
 import time
-time.sleep(1)
 
-with open('/proc/sys/kernel/ftrace_enabled', 'rb') as f:
-    curr = f.read()
+# first we try too large of a value, that detects older versions
+def to_large():
+    VALUE = b'9999999999999999999'
 
-print(curr)
-if curr in VALUE:
-    print('s1ngular1ty detected')
+    try:
+        with open('/proc/sys/kernel/ftrace_enabled', 'wb') as f:
+            f.write(VALUE)
+    except OSError:
+        return False
+
+    time.sleep(1)
+
+    with open('/proc/sys/kernel/ftrace_enabled', 'rb') as f:
+        curr = f.read()
+
+    print(curr)
+    return True
+
+# otherwise write a hex value, which singularity does't support
+# til you patch it :) hi!
+def with_hex():
+    try:
+        with open('/proc/sys/kernel/ftrace_enabled', 'wb') as f:
+            f.write(b'0xa')
+    except OSError:
+        return True
+
+    return False
+
+if to_large() or with_hex():
+    print('singularity detected')
 else:
-    print('weird behaviour, able to write invalid value to ftrace_enabled but its not preserved')
+    print('no tampering detected')
